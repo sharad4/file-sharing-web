812b228a8f0424a887a9445e7edb68d51e1fbb8d
Sharad Rao <sharad011989@gmail.com>
2024-07-09 16:38:43 +0530

    add social media

--- src/components/FileList.jsx
+++ src/components/FileList.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
+import { FacebookIcon, FacebookShareButton, LinkedinIcon, LinkedinShareButton, TwitterIcon, TwitterShareButton, WhatsappIcon, WhatsappShareButton } from 'react-share'
 
 const FileList = ({ files }) => {
-  // const files = [
-  //   { name: 'file1.txt', url: '/files/file1.txt' },
-  //   { name: 'file2.jpg', url: '/files/file2.jpg' },
-  // ];
+  
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
@@ -18,6 +16,36 @@ const FileList = ({ files }) => {
             >
               Download
             </a>
+            <div className="flex mt-4 space-x-2">
+              <FacebookShareButton
+                url={URL.createObjectURL(file)}
+                quote={`Check out this file: ${file.name}`}
+                hashtag='#FileShare'
+              >
+                <FacebookIcon size={32} round />
+              </FacebookShareButton>
+              <TwitterShareButton
+                url={URL.createObjectURL(file)}
+                title={`Check out this file: ${file.name}`}
+                hashtags={['FileShare']}
+              >
+                <TwitterIcon size={32} round/>
+              </TwitterShareButton>
+              <LinkedinShareButton
+                url={URL.createObjectURL(file)}
+                title={`Check out this file: ${file.name}`}
+                summary='I found this file interesting and wanted to share it with you.'
+                source='FileShare'
+              >
+                <LinkedinIcon size={32} round/>
+              </LinkedinShareButton>
+              <WhatsappShareButton
+                url={URL.createObjectURL(file)}
+                title={`Check out this file: ${file.name}`}
+              >
+                <WhatsappIcon size={32} round />
+              </WhatsappShareButton>
+            </div>
         </div>
       ))}
     </div>
